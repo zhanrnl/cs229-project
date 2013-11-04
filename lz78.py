8c2654d5b4cf6013e62cf82f70148920f3c8a7ee
Matt Staib <mstaib@stanford.edu>
2013-11-04 14:47:59 -0800

    Added functions for distance metrics from the paper

--- lz78.py
+++ lz78.py
@@ -15,3 +15,17 @@ def k_est(s):
         Kest += 1
 
     return Kest
+
+def k_concat(x, y):
+    return float(k_est(x + y) + k_est(y + x)) / 2
+
+def k_cond(x, y):
+    return k_concat(x, y) - k_est(y)
+
+def metric_d1(x, y):
+    assert(len(x) > 0 or len(y) > 0)
+    return float(k_cond(x, y) + k_cond(y, x)) / k_concat(x, y)
+
+def metric_d2(x, y):
+    assert(len(x) > 0 or len(y) > 0)
+    return float(max(k_cond(x, y), k_cond(y, x))) / max(k_est(x), k_est(y))
