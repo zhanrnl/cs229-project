8775865a9741641d946c19883d3726a23c0b45ed
Matt James Staib <mstaib@corn13.stanford.edu>
2013-12-08 14:59:07 -0800

    HOLY CRAP IT WORKS JF:OIJWE:OFIJ:O

--- metric_learn.py
+++ metric_learn.py
@@ -7,6 +7,7 @@ import cPickle
 import scipy.io
 import subprocess
 from sklearn.decomposition import PCA
+from sklearn.neighbors import BallTree, DistanceMetric
 
 def mat_to_col_major_order(a):
     return a.reshape((a.size, 1), order='F')
@@ -84,17 +85,11 @@ def build_a_b_pairs_vector(n = 2, num_blocks = 6):
 
     return pairs
 
-def metric_learn():
-    pairs = build_a_b_pairs_vector(2, 6)
-    n_train = int(round(0.7 * len(pairs)))
-
-    pairs_train = pairs[:n_train]
-    pairs_train_flat = [item for subtuple in pairs_train for item in subtuple]
-    
-    pca = PCA(n_components = 35)
+def metric_learn(pairs_test, pairs_train, n_components = 35):
+    pca = PCA(n_components = n_components)
     pca.fit(pairs_train_flat)
 
-    pairs_flat = [item for subtuple in pairs_train for item in subtuple]
+    pairs_flat = [item for subtuple in pairs for item in subtuple]
     pairs_pca_flat = pca.transform(pairs_flat)
     
     pairs_pca = list()
@@ -104,6 +99,7 @@ def metric_learn():
         pairs_pca.append((pairs_pca_flat[a], pairs_pca_flat[b]))
     
     pairs_pca_train = pairs_pca[:n_train]
+    pairs_pca_test = pairs_pca[n_train:]
 
     ys = ys_from_pairs(pairs_pca_train)
 
@@ -112,3 +108,47 @@ def metric_learn():
     print 'finished running cvx'
 
     M = load_cvx_result()
+
+    return pairs_pca_train, pairs_pca_test, M
+
+def neighbors(n_components = 35, fraction = 0.10):
+    pairs_pca_train, pairs_pca_test, M = metric_learn(n_components)
+
+    dist = DistanceMetric.get_metric('mahalanobis', VI = M)
+
+    a_sections = [x[0] for x in pairs_pca_test]
+    b_sections = [x[1] for x in pairs_pca_test]
+
+    a_sections_train = [x[0] for x in pairs_pca_train]
+    b_sections_train = [x[1] for x in pairs_pca_train]
+    
+    bt = BallTree(a_sections, metric = dist)
+    bt_train = BallTree(a_sections_train, metric = dist)
+    bt_euc = BallTree(a_sections)
+    bt_euc_train = BallTree(a_sections_train)
+
+    top_fraction = int(len(b_sections) * fraction)
+    top_fraction_train = int(len(b_sections_train) * fraction)
+
+    res = bt.query(b_sections, top_fraction)
+    res_train = bt_train.query(b_sections_train, top_fraction_train)
+    res_euc = bt_euc.query(b_sections, top_fraction)
+    res_euc_train = bt_euc_train.query(b_sections_train, top_fraction_train)
+
+    c = 0
+    c_euc = 0
+    for i in xrange(len(b_sections)):
+        if i in res[1][i]:
+            c += 1
+        if i in res_euc[1][i]:
+            c_euc += 1
+
+    c_train = 0
+    c_euc_train = 0
+    for i in xrange(len(b_sections_train)):
+        if i in res_train[1][i]:
+            c_train += 1
+        if i in res_euc_train[1][i]:
+            c_euc_train += 1
+
+    return ((c, c_euc, len(b_sections)), (c_train, c_euc_train, len(b_sections_train)))
