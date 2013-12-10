3b303512971a123203ab1f01e8aa6f83428eaba6
Lennart <zhanrnl@gmail.com>
2013-12-10 15:10:53 -0800

    Improve svm classify for running a large job with cross validation

--- svm_classify.py
+++ svm_classify.py
@@ -47,9 +47,9 @@ def load_pickled((i, d, n)):
           num_not_split += 1
           continue
 
-        f_vecs.append(features_multibar_split(a, d, n))
+        f_vecs.append(double_feature_vec(a, d, n))
         types.append(0)
-        f_vecs.append(features_multibar_split(b, d, n))
+        f_vecs.append(double_feature_vec(b, d, n))
         types.append(1)
 
     print '{} tunes successfully split, {} did not split, {} '\
@@ -102,42 +102,12 @@ def train_test_split(f_vecs, types, fraction = 0.7):
     return tuple([np.array(x) for x in t])
 
 
-def a_b_classify(n = 2, num_blocks = 6):
+def a_b_classify_pca((f_train, t_train, f_test, t_test, n_components)):
     '''
     Uses an SVM to classify A and B sections based on the feature vectors
     built above, and returns some statistical results
     '''
-    print '{0}: Building feature vectors'.format(time.ctime())
-    f_vecs, types = build_a_b_features_labels(n = n, num_blocks = num_blocks)
-
-    f_train, t_train, f_test, t_test = train_test_split(f_vecs, types, fraction = 0.9)
-
-    print '{0}: Training the SVM'.format(time.ctime())
-    clf = svm.SVC()
-    clf.fit(f_train, t_train)
-
-    t_predict = clf.predict(f_test)
-    t_train_predict = clf.predict(f_train)
-    
-    print 'Confusion matrix is built so that C_ij is the number of observations known to be in group i but predicted to be in group j. In this case, group 0 corresponds to A sections and group 1 corresponds to B sections.'
-    
-    print 'Confusion matrix on test data:'
-    print confusion_matrix(t_test, t_predict)
-
-    print 'Confusion matrix on training data:'
-    print confusion_matrix(t_train, t_train_predict)
-
-def a_b_classify_pca(n = 2, num_blocks = 6, n_components = 35):
-    '''
-    Uses an SVM to classify A and B sections based on the feature vectors
-    built above, and returns some statistical results
-    '''
-    print '{0}: Building feature vectors'.format(time.ctime())
-    f_vecs, types = build_a_b_features_labels(n = n, num_blocks = num_blocks)
-
-    f_train, t_train, f_test, t_test = train_test_split(f_vecs, types, fraction = 0.9)
-
-    print '{0}: Starting PCA (this could take a while...)'.format(time.ctime())
+    print '{}: Starting PCA with {} components (this could take a while...)'.format(time.ctime(), n_components)
     pca = PCA(n_components = n_components)
     pca.fit(f_train)
     f_train_pca = list(pca.transform(f_train))
@@ -147,19 +117,67 @@ def a_b_classify_pca(n = 2, num_blocks = 6, n_components = 35):
     clf = svm.SVC()
     clf.fit(f_train_pca, t_train)
 
+    print '{0}: Classifying using SVM'.format(time.ctime())
     t_predict = clf.predict(f_test_pca)
     t_train_predict = clf.predict(f_train_pca)
     
     print 'Confusion matrix is built so that C_ij is the number of observations known to be in group i but predicted to be in group j. In this case, group 0 corresponds to A sections and group 1 corresponds to B sections.'
     
     print 'Confusion matrix on test data:'
-    print confusion_matrix(t_test, t_predict)
+    test_confuse = confusion_matrix(t_test, t_predict)
+    print test_confuse
 
     print 'Confusion matrix on training data:'
-    print confusion_matrix(t_train, t_train_predict)
+    train_confuse = confusion_matrix(t_train, t_train_predict)
+    print train_confuse
+    return train_confuse, test_confuse
     
+def split_data(all_pairs, i, k):
+    num_pairs = len(all_pairs)
+    i = float(i)
+    start_split = int(i / k * num_pairs)
+    end_split = int((i+1) / k * num_pairs)
+    train = all_pairs[0:start_split] + all_pairs[end_split:]
+    test = all_pairs[start_split:end_split]
+    return train, test
 
 if __name__ == '__main__':
-  #d = get_durations()
-  #print d
-  a_b_classify(n = 2, num_blocks = 6)
+  print '{0}: Building feature vectors'.format(time.ctime())
+  f_vecs, types = build_a_b_features_labels(n = 2, num_blocks = 6)
+
+  for n_components in [3, 5, 10, 20, 35]:
+    print "Classifying with {} components in PCA".format(n_components)
+    splits = []
+    k = 8
+    for i in range(k):
+      f_train, f_test = split_data(f_vecs, i, k)
+      t_train, t_test = split_data(types, i, k)
+      splits.append((f_train, t_train, f_test, t_test, n_components))
+    pool = Pool()
+    map_result = pool.map_async(a_b_classify_pca, splits, 1)
+    pool.close()
+    pool.join()
+    result = map_result.get()
+
+    train_confuse_sum = np.zeros((2, 2))
+    test_confuse_sum = np.zeros((2, 2))
+    for train_confuse, test_confuse in result:
+      #print train_confuse
+      #print test_confuse
+      #print type(train_confuse)
+      #print type(test_confuse)
+      #print str(train_confuse)
+      #print str(test_confuse)
+      train_confuse_sum = train_confuse_sum + train_confuse
+      test_confuse_sum = test_confuse_sum + test_confuse
+    #print train_confuse_sum
+    #print test_confuse_sum
+    #print type(train_confuse_sum)
+    #print type(test_confuse_sum)
+    #print str(train_confuse_sum)
+    #print str(test_confuse_sum)
+    with open("svm_result_{}".format(n_components), 'w') as f:
+      f.write(str(train_confuse_sum))
+      f.write('\n')
+      f.write(str(test_confuse_sum))
+
